d8bca2b23ae78ad4c8b85aa7c8ef86727241d049
Prajukta Dey <90215092+prajuktadey@users.noreply.github.com>
2023-01-10 23:52:58 +0530

    Added the concatenation files.

---
+++ Concat.sql
@@ -0,0 +1,13 @@
+SELECT 'This ' || 'and ' || 'that';
+-- string or string or string so it returns FALSE
+
+SELECT CONCAT('This ', 'and ', 'that');
+
+SELECT CONCAT('Love', ' ', 'is', ' ', 'all', ' ', 'you', ' ', 'need');
+
+SELECT CONCAT('one', 'two');
+
+SELECT CONCAT('string', 42);
+
+SELECT CONCAT(42, 42);
+
