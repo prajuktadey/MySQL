d5c38f0841a14f866f7398cb0e4246c47c7e8c44
Prajukta Dey <90215092+prajuktadey@users.noreply.github.com>
2023-01-10 23:32:50 +0530

    Added the case statements scripts.

---
+++ Case Statement.sql
@@ -0,0 +1,20 @@
+USE scratch;
+
+DROP TABLE IF EXISTS booltest;
+CREATE TABLE booltest (a INTEGER, b INTEGER);
+INSERT INTO booltest VALUES (1, 0);
+SELECT * FROM booltest;
+
+SELECT
+  CASE WHEN a < 5 THEN 'true' ELSE 'false' END AS boolA,
+  CASE WHEN b > 0 THEN 'true' ELSE 'false' END AS boolB
+  FROM booltest
+;
+
+SELECT
+  CASE a WHEN 1 THEN 'true' ELSE 'false' END AS boolA,
+  CASE b WHEN 1 THEN 'true' ELSE 'false' END AS boolB 
+  FROM booltest
+;
+
+DROP TABLE IF EXISTS booltest;
