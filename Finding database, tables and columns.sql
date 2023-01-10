bbfb679b34a1acfa9e835672bd11a34aa2205cf6
Prajukta Dey <90215092+prajuktadey@users.noreply.github.com>
2023-01-10 21:21:37 +0530

    Added the finding database scripts.

---
+++ Finding database, tables and columns.sql
@@ -0,0 +1,11 @@
+USE scratch;
+
+SHOW DATABASES;
+-- This will show all the databases connected to the server
+
+USE scratch;
+SHOW tables;
+-- This will show the tables present in the scratch database
+
+DESCRIBE customer;
+-- This will give a list of columns in a table along with their type
