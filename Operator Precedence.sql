caca6d0cc577be2f1c7603f5bb256602f8e1dce2
Prajukta Dey <90215092+prajuktadey@users.noreply.github.com>
2023-01-10 23:29:52 +0530

    Added the file on operator precedence.

---
+++ Operator Precedence.sql
@@ -0,0 +1,13 @@
+SELECT 5 + 6 * 7 - 8;
+
+SELECT (5 + 6) * (7 - 8);
+
+SELECT 5 + (6 * 7) - 8;
+
+-- PEDMAS
+-- 1. Parentheses
+-- 2. Exponents
+-- 3. Division
+-- 4. Multiplication
+-- 5. Addition
+-- 6. Subtraction
