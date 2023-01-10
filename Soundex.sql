d0073c513f641b87545cf8171d59438f4e702b90
Prajukta Dey <90215092+prajuktadey@users.noreply.github.com>
2023-01-11 00:12:51 +0530

    Added the soundex scripts.

---
+++ Soundex.sql
@@ -0,0 +1,7 @@
+SELECT SOUNDEX('bill'), SOUNDEX('bell');
+
+SELECT SOUNDEX('acton'), SOUNDEX('action');
+
+SELECT SOUNDEX('acting'), SOUNDEX('action');
+
+SELECT 'bill' SOUNDS LIKE 'boil', 'bill' SOUNDS LIKE 'phil';
